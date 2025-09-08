b0c806261eb36132052580b8e53bbe641dc135d3
RaiAnish15 <114273678+RaiAnish15@users.noreply.github.com>
2025-09-08 16:07:11 +0530

    Update test-1.py

--- test-1.py
+++ test-1.py
@@ -4,7 +4,7 @@ import streamlit as st
 st.set_page_config(page_title="Class 1 – Streamlit Basics")
 
 st.title("Hello Streamlit 👋")
-st.write("This is our very first app — a Python script that renders a UI.")
+st.write("This is test-1")
 
 name = st.text_input("Enter your name:", "Anish")
 st.write(f"Welcome, **{name}**!")
