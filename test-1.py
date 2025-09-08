364a2113a37993ea696785938f5866347ee8c828
RaiAnish15 <114273678+RaiAnish15@users.noreply.github.com>
2025-09-08 16:07:47 +0530

    Update test-1.py

--- test-1.py
+++ test-1.py
@@ -6,6 +6,6 @@ st.set_page_config(page_title="Class 1 – Streamlit Basics")
 st.title("Hello Streamlit 👋")
 st.write("This is test-1")
 
-name = st.text_input("Enter your name:", "Anish")
+name = st.text_input("Enter your name:", "Enter Your Name")
 st.write(f"Welcome, **{name}**!")
 
