dd17c3cc8a24d2a8e447edd7d7eb3764cd9d6c43
RaiAnish15 <114273678+RaiAnish15@users.noreply.github.com>
2025-09-08 16:06:11 +0530

    Update test-1.py

--- test-1.py
+++ test-1.py
@@ -1,7 +1,7 @@
 # app.py
 import streamlit as st
 
-st.set_page_config(page_title="Class 1 – Streamlit Basics", layout="wide")
+st.set_page_config(page_title="Class 1 – Streamlit Basics")
 
 st.title("Hello Streamlit 👋")
 st.write("This is our very first app — a Python script that renders a UI.")
