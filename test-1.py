08235e437ce5da3f7c0c8d2c096965d29fb45b77
RaiAnish15 <114273678+RaiAnish15@users.noreply.github.com>
2025-09-22 20:44:05 +0530

    Update test-1.py

--- test-1.py
+++ test-1.py
@@ -1,10 +1,9 @@
-# app.py
 import streamlit as st
 
 st.set_page_config(page_title="Class 1 – Streamlit Basics")
 
-st.title("Hello Streamlit 👋")
-st.write("This is test-1")
+st.title("Hello! Welcome to Streamlit. This is test-1")
+
 
 name = st.text_input("Enter your name:","Enter your Name")
 st.write(f"Welcome, **{name}**!")
